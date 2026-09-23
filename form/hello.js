2a77528893f0db0ddcacb5a13020835a1967d5f1
HoThiTuyet07 <hothituyet.121207@gmail.com>
2026-09-23 20:43:45 +0700

    Delete form directory

--- form/hello.js
+++
@@ -1,8 +0,0 @@
-function sum() {
-    let a = document.getElementById("soa").value;
-    let b = document.getElementById("sob").value;
-    let result = parseFloat(a) + parseFloat(b);
-    document.getElementById("kq").innerHTML = `Kết quả: ${result}`;
-    
-}
-sum();
